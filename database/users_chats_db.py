9b9007cf6798124f93ac9a5be26a937c5773c8fd
chaturbihari <shivcollegelife@gmail.com>
2025-07-23 13:05:18 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -1,21 +1,15 @@
-# bot/database/users_chats_db.py
+# database/users_chats_db.py
 
 import motor.motor_asyncio
-from info import DATABASE_NAME, DATABASE_URI, IMDB, IMDB_TEMPLATE, MELCOW_NEW_USERS, P_TTI_SHOW_OFF, SINGLE_BUTTON, SPELL_CHECK_REPLY, PROTECT_CONTENT, AUTO_DELETE, MAX_BTN, AUTO_FFILTER, SHORTLINK_API, SHORTLINK_URL, IS_SHORTLINK, TUTORIAL, IS_TUTORIAL
-
-_db_instance = None  # This will hold the initialized db object
-
-def get_db():
-    if _db_instance is None:
-        raise Exception("Database not initialized. Call await init_db() first.")
-    return _db_instance
-
-async def init_db():
-    global _db_instance
-    _db_instance = Database(DATABASE_URI, DATABASE_NAME)
-
+from info import (
+    DATABASE_NAME, DATABASE_URI, IMDB, IMDB_TEMPLATE, MELCOW_NEW_USERS,
+    P_TTI_SHOW_OFF, SINGLE_BUTTON, SPELL_CHECK_REPLY, PROTECT_CONTENT,
+    AUTO_DELETE, MAX_BTN, AUTO_FFILTER, SHORTLINK_API, SHORTLINK_URL,
+    IS_SHORTLINK, TUTORIAL, IS_TUTORIAL
+)
 
 class Database:
+    
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
@@ -23,10 +17,18 @@ class Database:
         self.grp = self.db.groups
 
     def new_user(self, id, name):
-        return dict(id=id, name=name, ban_status={"is_banned": False, "ban_reason": ""})
+        return dict(
+            id=id,
+            name=name,
+            ban_status=dict(is_banned=False, ban_reason="")
+        )
 
     def new_group(self, id, title):
-        return dict(id=id, title=title, chat_status={"is_disabled": False, "reason": ""})
+        return dict(
+            id=id,
+            title=title,
+            chat_status=dict(is_disabled=False, reason="")
+        )
 
     async def add_user(self, id, name):
         user = self.new_user(id, name)
@@ -40,13 +42,13 @@ class Database:
         return await self.col.count_documents({})
 
     async def remove_ban(self, id):
-        await self.col.update_one({'id': id}, {'$set': {'ban_status': {'is_banned': False, 'ban_reason': ''}}})
+        await self.col.update_one({'id': id}, {'$set': {'ban_status': dict(is_banned=False, ban_reason='')}})
 
     async def ban_user(self, user_id, ban_reason="No Reason"):
-        await self.col.update_one({'id': user_id}, {'$set': {'ban_status': {'is_banned': True, 'ban_reason': ban_reason}}})
+        await self.col.update_one({'id': user_id}, {'$set': {'ban_status': dict(is_banned=True, ban_reason=ban_reason)}})
 
     async def get_ban_status(self, id):
-        default = {"is_banned": False, "ban_reason": ""}
+        default = dict(is_banned=False, ban_reason='')
         user = await self.col.find_one({'id': int(id)})
         return user.get('ban_status', default) if user else default
 
@@ -71,7 +73,7 @@ class Database:
         return chat.get('chat_status') if chat else False
 
     async def re_enable_chat(self, id):
-        await self.grp.update_one({'id': int(id)}, {'$set': {'chat_status': {"is_disabled": False, "reason": ""}}})
+        await self.grp.update_one({'id': int(id)}, {'$set': {'chat_status': dict(is_disabled=False, reason="")}})
 
     async def update_settings(self, id, settings):
         await self.grp.update_one({'id': int(id)}, {'$set': {'settings': settings}})
@@ -98,7 +100,7 @@ class Database:
         return chat.get('settings', default) if chat else default
 
     async def disable_chat(self, chat, reason="No Reason"):
-        await self.grp.update_one({'id': int(chat)}, {'$set': {'chat_status': {"is_disabled": True, "reason": reason}}})
+        await self.grp.update_one({'id': int(chat)}, {'$set': {'chat_status': dict(is_disabled=True, reason=reason)}})
 
     async def total_chat_count(self):
         return await self.grp.count_documents({})
@@ -108,3 +110,17 @@ class Database:
 
     async def get_db_size(self):
         return (await self.db.command("dbstats"))['dataSize']
+
+
+# The magic: lazy singleton DB instance
+import asyncio
+
+db = None
+
+async def _init_db():
+    global db
+    if db is None:
+        db = Database(DATABASE_URI, DATABASE_NAME)
+
+# Automatically run _init_db at import time
+asyncio.get_event_loop().run_until_complete(_init_db())
